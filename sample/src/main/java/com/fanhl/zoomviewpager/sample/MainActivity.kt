18f3c0896438a2a11ae28a78e5240c813641a25f
fanhl <fanhonglin@klicen.com>
2018-11-14 11:34:18 +0800

    layoutManager

--- sample/src/main/java/com/fanhl/zoomviewpager/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/zoomviewpager/sample/MainActivity.kt
@@ -2,20 +2,28 @@ package com.fanhl.zoomviewpager.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val adapter = MainAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view.layoutManager = LinearLayoutManager(this)
+        PagerSnapHelper().attachToRecyclerView(recycler_view)
 
-        recycler_view.adapter = MainAdapter()
+        recycler_view.adapter = adapter
+
+        adapter.setNewData(
+            List(10) {
+                "$it"
+            }
+        )
     }
 
     class MainAdapter : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_view) {
