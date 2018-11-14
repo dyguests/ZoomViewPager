6f35c0508739f12c800a3dcf0f7800b974efbcb7
fanhl <fanhonglin@klicen.com>
2018-11-14 14:22:46 +0800

    ZoomLayoutManager

---
+++ library/src/main/java/com/fanhl/zoomviewpager/ZoomLayoutManager.kt
@@ -0,0 +1,6 @@
+package com.fanhl.zoomviewpager
+
+import android.support.v7.widget.RecyclerView
+
+abstract class ZoomLayoutManager : RecyclerView.LayoutManager() {
+}
--- library/src/main/java/com/fanhl/zoomviewpager/ZoomSnapHelper.kt
+++
@@ -1,4 +0,0 @@
-package com.fanhl.zoomviewpager
-
-class ZoomSnapHelper {
-}
