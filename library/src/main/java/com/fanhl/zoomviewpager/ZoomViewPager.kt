116b3e03ca343cfc4b466594f738ee3ff9e76437
fanhl <fanhonglin@klicen.com>
2018-11-14 09:49:07 +0800

    onMeasure

--- library/src/main/java/com/fanhl/zoomviewpager/ZoomViewPager.kt
+++ library/src/main/java/com/fanhl/zoomviewpager/ZoomViewPager.kt
@@ -2,6 +2,7 @@ package com.fanhl.zoomviewpager
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
 
 /**
@@ -12,12 +13,28 @@ class ZoomViewPager @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr) {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+//        val specSizeWidth = View.MeasureSpec.getSize(widthMeasureSpec)
+//        val specSizeHeight = View.MeasureSpec.getSize(heightMeasureSpec)
+//        setMeasuredDimension(specSizeWidth, specSizeHeight)
+
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            this.measureChild(child, widthMeasureSpec, heightMeasureSpec)
+        }
+    }
+
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        val paddedWidth = width - paddingLeft - paddingRight
+        val paddedHeight = height - paddingTop - paddingBottom
+
 
         for (i in 0 until childCount) {
             val child = getChildAt(i)
 
-            child.layout(l, t, r, b)
+            child.layout(paddingLeft, paddingTop, paddingLeft + paddedWidth, paddingTop + paddedHeight)
         }
     }
 }
