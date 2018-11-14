30d3a0a924605711c0224cb1de49d4625660a5ec
fanhl <fanhonglin@klicen.com>
2018-11-14 09:42:13 +0800

    good

--- library/src/main/java/com/fanhl/zoomviewpager/ZoomViewPager.kt
+++ library/src/main/java/com/fanhl/zoomviewpager/ZoomViewPager.kt
@@ -1,4 +1,23 @@
 package com.fanhl.zoomviewpager
 
-class ZoomViewPager {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+/**
+ * 可以缩放的ViewPager
+ */
+class ZoomViewPager @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ViewGroup(context, attrs, defStyleAttr) {
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+
+            child.layout(l, t, r, b)
+        }
+    }
 }
--- sample/src/main/java/com/fanhl/zoomviewpager/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/zoomviewpager/sample/MainActivity.kt
@@ -2,11 +2,13 @@ package com.fanhl.zoomviewpager.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ViewPager::clearOnPageChangeListeners
     }
 }
